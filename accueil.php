7ae896b28598127e8b5b69bc411033a09cb1758f
Fawzi BAZARI <fawzibazari@iMac-de-IFCV-3.local>
2021-01-26 18:06:00 +0100

    testo

--- accueil.php
+++ accueil.php
@@ -35,20 +35,49 @@
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
-</head>
-<div class="header">
-    <a href="#default" class="logo">#Embauchez-Moi</a>
-    <div class="header-right">
-      <a  href="index.html">Acceuil</a>
-      <a class="active" href="#">Nos étudiants</a>
-      <a href="#">Contact</a>
+  <header id="header">
+    <div class="container d-flex">
+
+      <div class="logo me-auto">
+        <h1 class="text-light"><a href="index.html"><span>Embauchez Moi</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu float-right d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.html">Accueil</a></li>
+          <li><a href="accueil.php">Nos étudiant</a></li>
+          <!-- <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#team">Equipe</a></li> -->
+          <!-- <li class="drop-down"><a href="">Drop Down</a> -->
+            <ul>
+              <!-- <li><a href="#">Drop Down 1</a></li>
+              <li class="drop-down"><a href="#">Drop Down 2</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Drop Down 5</a></li>
+            </ul> -->
+          </li>
+          <!-- <li><a href="#contact">Contact Us</a></li> -->
+        </ul>
+      </nav><!-- .nav-menu -->
+
     </div>
-  </div>
-<body style="background-color: #f8f9fa;">
-<section style="margin-left: 60px; margin-right: 60px;">
+  </header>
+</head>
 
 
-    
+
         <?php
 $conn=new mysqli($servername,$username,$password,$dbname);
 
@@ -69,13 +98,13 @@ foreach ($res as $rows):
 ?>
 
 
-                      <div class="col" style="margin-top: 5%;" >
-                <div class="card-container">
+                      <div class="ta col" style="margin-top: 5%;" >
+                <div class="to card-container">
                     <span class="pro"><i style="font-size:30px" class="fa">&#xf07a;</i></span>
                 
                     <img class="round" src="<?php echo 'Photo-neon/' . $rows['Image_Profile']?>"  alt="user" />
-                    <h3><?php echo $rows['Prenom'] ?> </h3>
-                    <h6><i class="fas fa-map-marker-alt"></i>  <?php echo $rows['Address']?> </h6>
+                    <h3 class="yo"><?php echo $rows['Prenom'] ?> </h3>
+                    <h6 class="yi"><i class="fas fa-map-marker-alt"></i>  <?php echo $rows['Address']?> </h6>
                     <p><?php echo $rows['nom'] ?>  <br/> front-end developer</p>
                     <div class="buttons" style="padding-bottom:10% ;">
                     
@@ -103,9 +132,11 @@ endforeach;
 
 </section>
 </body>
+
+ <!-- ======= Footer ======= -->
  <footer id="footer">
     <div class="footer-top">
-      <div class="tocontainer">
+      <div class="container">
         <div class="row">
 
           <div class="col-lg-3 col-md-6 footer-info">
@@ -155,206 +186,36 @@ endforeach;
       </div>
     </div>
 
-    <div class="tesrcontainer">
+    <div class="container">
       <div class="copyright">
         &copy; Copyright <strong><span>IMIE-Paris</span></strong>.Tout les droit son réserver
       </div>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/mamba-one-page-bootstrap-template-free/ -->
+
       </div>
     </div>
   </footer><!-- End Footer -->
 
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
 <html>
 
-<style> 
-/* @import url('https://fonts.googleapis.com/css?family=Montserrat');
-                
-                * {
-                    box-sizing: border-box;
-                }
-                        img {
-                max-width:100%;
-                height:auto;
-                }   
-                        
-                .container {
-                    background-color: blue;
-                    font-family: Montserrat, sans-serif;
-                    
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    flex-direction: column;
-                
-                    min-height: 100vh;
-                    margin-left: 300px;
-                }
-                
-                h3 {
-                    margin: 10px 0;
-                    color: black;
-                }
-                
-                h6 {
-                    margin: 5px 0;
-                    text-transform: uppercase;
-                    color: black;
-                }
-                
-                p {
-                    font-size: 14px;
-                    line-height: 21px;
-                    color: black;
-                }
-                
-                .card-container {
-                    background-color: white;
-                    border-radius: 5px;
-                    box-shadow: 0px 10px 20px -10px rgba(0,0,0,0.75);
-                    color: #B3B8CD;
-                    padding-top: 30px;
-                    position: relative;
-                    width: 350px;
-                    max-width: 100%;
-                    text-align: center;
-                }
-
-                .card-container .pro {
-                    color: #231E39;
-                    background-color: #009fe4;
-                    border-radius: 3px;
-                    font-size: 14px;
-                    font-weight: bold;
-                    padding: 3px 7px;
-                    position: absolute;
-                    top: 30px;
-                    left: 30px;
-                }
-                
-                
-                
-                .card-container .round {
-                    border: 1px solid white;
-                    border-radius: 50%;
-                    padding: 7px;
-                }
-                
-                button.primary {
-                    background-color: #e02941;
-                    border: 1px solid white;
-                    border-radius: 3px;
-                    color: #231E39;
-                    font-family: Montserrat, sans-serif;
-                    font-weight: 500;
-                    padding: 10px 25px;
-                }
-                
-                button.primary.ghost {
-                    background-color: transparent;
-                    color: #02899C;
-                }
-                
-                .skills {
-                    background-color: white;
-                    text-align: left;
-                    padding: 15px;
-                    margin-top: 30px;
-                    color: black;
-                }
-                
-                .skills ul {
-                    list-style-type: none;
-                    margin: 0;
-                    padding: 0;
-                    color: black;
-                }
-                
-                .skills ul li {
-                    border: 1px solid #2D2747;
-                    border-radius: 2px;
-                    display: inline-block;
-                    font-size: 12px;
-                    margin: 0 7px 7px 0;
-                    padding: 7px;
-                    color: black;
-                }
-                
-                footer {
-                    background-color: white;
-                    color: #fff;
-                    font-size: 14px;
-                    bottom: 0;
-                    position: fixed;
-                    left: 0;
-                    right: 0;
-                    text-align: center;
-                    z-index: 999;
-                }
-                
-                footer p {
-                    margin: 10px 0;
-                }
-                
-                footer i {
-                    color: red;
-                }
-                
-                footer a {
-                    color: #3c97bf;
-                    text-decoration: none;
-                }
-                .header {
-                overflow: hidden;
-                background-color:#E02941;
-                padding: 20px 10px;
-                }
-                
-                .header a {
-                float: left;
-                color: white;
-                text-align: center;
-                padding: 12px;
-                text-decoration: none;
-                font-size: 18px; 
-                line-height: 25px;
-                border-radius: 4px;
-                }
-                
-                .header a.logo {
-                font-size: 25px;
-                font-weight: bold;
-                }
-                
-                .header a:hover {
-                background-color: dodgerblue;
-                color: white;
-                }
-                
-                .header a.active {
-                background-color: dodgerblue;
-                color: white;
-                }
-                
-                .header-right {
-                float: right;
-                }
-                @media screen and (max-width: 500px) {
-                .header a {
-                float: none;
-                display: block;
-                text-align: left;
-                }
-                
-                .header-right {
-                float: none;
-                }
-                
-                } */
-</style>
 
 
 
